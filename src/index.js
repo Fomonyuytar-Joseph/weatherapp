aa0ba3a9d7d4e92b89b3a82f2b20dacdc91cdf5e
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-06-22 08:11:40 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
 
+
+class App extends React.Component{
+  render(){
     window.navigator.geolocation.getCurrentPosition(
-        (position) => { console.log(position) },
-        (error) => { console.log(error) }
+      (position) => { console.log(position) },
+      (error) => { console.log(error) }
     )
-  return (<div>
-
-
-    Hi there
-  </div>);
-};
+    return <div>Latitude: </div>
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
