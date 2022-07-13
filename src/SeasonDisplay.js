b1419ff17720209271fc1b034caa543319c901d3
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2022-07-13 13:24:57 +0100

    init

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,25 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const getSeason= (lat ,month)=>{
-
-  if( month>2 && month<9 ){
-   return  lat > 0 ?  'summer' : 'winter';
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
   }
-
-  else{
-    return  lat > 0 ? 'winter': 'summer'
-  }
-
-
-}
+};
 
 function SeasonDisplay(props) {
-
-  const season=getSeason(props.lat ,new Date().getMonth())
-  console.log(season)
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text= season === "winter" ? "places are cold" : "Let go to Limbe beach"
+  // console.log(season);
   return (
-    <div>Season Display</div>
-  )
+    <div>
+     <h1> {text} </h1>
+    </div>
+  );
 }
 
-export default SeasonDisplay
+export default SeasonDisplay;
